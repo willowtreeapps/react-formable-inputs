391b1020215ab14b9ace7458d58f7d23b8251100
Trevor Senior <trevor@tsenior.com>
2016-01-14 09:15:26 -0500

    Add `IdentityWrapper` component.
    Feedback would be lovely.
    
    The basis of this component is that it will wrap other components, and
    ensure that they have a `getValue()` method associated with
    them. Currently, it acts as a pass-through for components that already
    implement their own `getValue()` functions, and handles basic DOM form
    components (input, select, textarea).
    
    Usage of this compnent can be seen in the test cases.
    
    - - -
    
    Implementation:
    
    So, this is a bit of a woozy and it's due to the way input type radio is
    implemented (and possibly others?). Instead of accepting only one
    child for IdentityWrapper, we now have to consider multiple
    children. e.g.
    
        <IdentityWrapper>
            <input type="radio" name="foo" value="cat" />
            <input type="radio" name="foo" value="cat" />
        </IdentityWrapper>
    
    Because of this, the implementation maps over `this.refs`, and pulls out
    the values of each child. If it is an invalid node passed in, it returns
    undefined as a value for that child. In the case of radio inputs, it
    returns undefined for unchecked radio buttons and the value of the radio
    button if it is checked.
    
    After we have an array of values, filter out the undefined values. There
    should only be *zero or one* values in the array after filtering out
    undefined values. If zero, return undefined. If one, return that element
    from the values array (`values[0]`).
    
    The implementation of IdentityWrapper can only return one value from
    `getValue()`. A warning is issued if something like this is passed in:
    
        <IdentityWrapper>
            <input type="text" name="foo" />
            <input type="text" name="bar" />
        </IdentityWrapper>
    
    There are other warnings in place to help indicate to users why their
    input is returning undefined. Should we throw an Error vs. a warning in
    these cases?
    
    - - -
    
    This is a rough implementation but I _think_ it covers the majority of
    use cases. Have I missed any?

---
+++ src/wrappers/__tests__/identityWrapper-test.js
@@ -0,0 +1,123 @@
+jest.dontMock('../identityWrapper');
+
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+const IdentityWrapper = require('../identityWrapper').default;
+
+describe('IdentityWrapper', () => {
+    it('renders a basic text input', () => {
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <input type="text" />
+            </IdentityWrapper>
+        );
+
+        expect(iden.getValue()).toEqual('');
+
+        let input = TestUtils.findRenderedDOMComponentWithTag(iden, 'input');
+
+        input.value = 'basic';
+        expect(iden.getValue()).toEqual('basic');
+    });
+
+    it('renders a basic checkbox input', () => {
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <input type="checkbox" />
+            </IdentityWrapper>
+        );
+
+        expect(iden.getValue()).toEqual(false);
+
+        let input = TestUtils.findRenderedDOMComponentWithTag(iden, 'input');
+
+        input.checked = true;
+        expect(iden.getValue()).toEqual(true);
+    });
+
+    it('renders basic radio inputs', () => {
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <input type="radio"
+                       name="pets" value="CAT" />
+                <input type="radio"
+                       name="pets" value="DOG" />
+            </IdentityWrapper>
+        );
+
+        expect(iden.getValue()).toEqual(void 0);
+
+        let inputs = TestUtils.scryRenderedDOMComponentsWithTag(iden, 'input');
+
+        inputs[0].checked = true;
+        expect(iden.getValue()).toEqual('CAT');
+
+        inputs[1].checked = true;
+        expect(iden.getValue()).toEqual('DOG');
+    });
+
+    it('renders basic select (select-one)', () => {
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <select>
+                    <option value="CAT">Cat</option>
+                    <option value="DOG">Dog</option>
+                </select>
+            </IdentityWrapper>
+        );
+
+        // By default, selects the first option
+        expect(iden.getValue()).toEqual('CAT');
+
+        let select = TestUtils.findRenderedDOMComponentWithTag(iden, 'select');
+
+        select.options[1].selected = true;
+        expect(iden.getValue()).toEqual('DOG');
+    });
+
+    it('renders basic select (select-multiple)', () => {
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <select multiple>
+                    <option value="CAT">Cat</option>
+                    <option value="DOG">Dog</option>
+                </select>
+            </IdentityWrapper>
+        );
+
+        expect(iden.getValue()).toEqual([]);
+
+        let select = TestUtils.findRenderedDOMComponentWithTag(iden, 'select');
+
+        select.options[0].selected = true;
+        expect(iden.getValue()).toEqual(['CAT']);
+        select.options[1].selected = true;
+        expect(iden.getValue()).toEqual(['CAT', 'DOG']);
+    });
+
+    it('should handle components that supply their own `getValue()`', () => {
+        const SomeInput = React.createClass({
+            getValue: () => 'some-input-value',
+            render: () => <span />
+        });
+
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <SomeInput />
+            </IdentityWrapper>
+        );
+
+        expect(iden.getValue()).toEqual('some-input-value');
+    });
+
+    it('should return undefined on invalid components', () => {
+        const iden = TestUtils.renderIntoDocument(
+            <IdentityWrapper>
+                <div>Invalid Component</div>
+            </IdentityWrapper>
+        );
+
+        expect(iden.getValue()).toEqual(undefined);
+    });
+});
---
+++ src/wrappers/identityWrapper.js
@@ -0,0 +1,83 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import warning from 'warning';
+
+/**
+
+   Wraps any input component (either Formable components, or native DOM
+   components) and returns a new component that follows Formable's API.
+
+ */
+const IdentityWrapper = React.createClass({
+
+    displayName: 'IdentityWrapper',
+
+    propTypes: {
+        children: PropTypes.node
+    },
+
+    /**
+       Gets the values (that it can) from all child components. Does
+       not do any filtering or anything fancy.
+
+       @return {string|Array<string>} - String value, or array of
+       strings (e.g. multiple select)
+     */
+    getValue() {
+        const values = Object.keys(this.refs).map((refId) => {
+            const child = this.refs[refId];
+
+            // Non-DOM component that implements `getValue()`
+            if (typeof child.getValue === 'function') {
+                return child.getValue();
+            }
+
+            if (!child.tagName || !child.type) {
+                warning(false, `Components passed into ${this.constructor.displayName} must have a tagName and type associated with them`);
+                return void 0;
+            }
+
+            const tagName = child.tagName.toLowerCase();
+            const type = child.type.toLowerCase();
+
+            if (tagName === 'input') {
+                if (type === 'checkbox') return child.checked;
+                if (type === 'radio') {
+                    return child.checked ? child.value : void 0;
+                }
+                return child.value;
+            }
+
+            if (tagName === 'select') {
+                if (type === 'select-multiple') {
+                    const select = ReactDOM.findDOMNode(child);
+                    const options = Array.prototype.slice.call(select.options);
+
+                    return options.filter((option) => option.selected)
+                                  .map((option) => option.value);
+                }
+                return child.value;
+            }
+
+            if (tagName === 'textarea') {
+                return child.value;
+            }
+
+            warning(false, `Unsupported component ${tagName} in ${this.constructor.displayName}`);
+            return void 0;
+        }).filter((value) => typeof value !== 'undefined');
+
+        warning(values.length <= 1, `${this.constructor.displayName} should only wrap one input type. Returning only the first valid value`);
+
+        return values.length === 0 ? void 0 : values[0];
+    },
+
+    render() {
+        const children = React.Children.map(this.props.children, (child, i) =>
+            React.cloneElement(child, { ref: `input-${i}` }));
+
+        return <span>{children}</span>;
+    }
+});
+
+export default IdentityWrapper;
