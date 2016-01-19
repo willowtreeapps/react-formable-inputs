ff56839d20e647b13a9bc11ea5ee49242aa33299
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2016-01-19 11:33:23 -0500

    Starting work on textarea input

--- example/src/app.js
+++ example/src/app.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { Select } from 'react-formable-inputs';
+import { Select, Textarea } from 'react-formable-inputs';
 
 const options = [
     { value: 'one', label: 'One' },
     { value: 'two', label: 'Two' }
 ];
 
+function getStuff() {
+    return <div>
+            <Select options={options} defaultValue="one" />
+            <hr />
+            <Textarea defaultValue="default value"/>
+        </div>;
+}
+
 render(
-    <Select options={options} defaultValue="one" />,
+    getStuff(),
     document.getElementById('app')
 );
